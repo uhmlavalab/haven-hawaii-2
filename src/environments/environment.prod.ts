fec6cd2304183a7a037adb20ad01586793c90063
Ryan Theriot <ryanctheriot@gmail.com>
2018-03-07 21:58:29 -1000

    Firebase hosting setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBQAvBMesZU-9OV8rcWiuBKrxsU20PZz9g',
+    authDomain: 'haven-196001.firebaseapp.com',
+    databaseURL: 'https://haven-196001.firebaseio.com',
+    projectId: 'haven-196001',
+    storageBucket: 'haven-196001.appspot.com',
+    messagingSenderId: '753621836648'
+  }
 };
